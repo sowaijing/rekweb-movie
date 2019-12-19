42f1194f4e46f2d066517cfcb6f95d89970d0fd2
sowaijing <wjs1997@hotmail.com>
2019-12-19 23:32:27 +0800

    complete the detail page of movie

--- detail.php
+++ detail.php
@@ -16,8 +16,8 @@ $movie = json_decode($movie, true);
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <title>Rekweb Movie</title>
-  </head>
-  <body>
+  </head> 
+  <body> 
     <div class="container" >
       <div class="row">
         <div class="col"> 
